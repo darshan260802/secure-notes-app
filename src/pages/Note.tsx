76352b80cecad0c9e6ff0bfa9ce6fcafddffe4b8
Darshan Patel <darshanpatel2608ce@gmail.com>
2025-02-11 08:59:41 +0530

    base

--- src/pages/Note.tsx
+++ src/pages/Note.tsx
@@ -1,4 +1,23 @@
+import { useLayoutEffect, useState } from "react"
+import { getNotes } from "../api/note"
+import { useNavigate } from "react-router"
 
 export default function Note() {
+
+    const navigation = useNavigate()
+    const [notes, setNotes] = useState([])
+
+    useLayoutEffect(() => {
+        getNotes().then((data) => {
+
+        }).catch((error) => {
+            if(error.status && error.status === 401) {
+                navigation('/auth');
+                return;
+            }
+            console.error(error)
+        })
+    }, [])
+
     return <h1>Note</h1>
 }
